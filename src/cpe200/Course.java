c74b74806d98a61f9e49fc483fde6a3b853ea0bf
Autapan <armindy01@gmail.com>
2016-09-09 13:17:16 +0700

    Add

--- src/cpe200/Course.java
+++ src/cpe200/Course.java
@@ -3,25 +3,50 @@ package cpe200;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class Course {
 
     public Course() {
         this("","","",30);
+      /*  course_name="TBA";
+        course_id="000000";
+        lecturer="TBA";
+        max_students=30;
+        no_students=0;*/
     }
 
     public Course(String n, String cid) {
         // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
+        this.course_id=!isValidCourse_id(cid)?"000000":cid;
+        lecturer="TBA";
+        max_students=30;
+        no_students=0;
     }
 
     public Course(String n, String cid, String l) {
         // implement here
+        this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
+        this.course_id=!isValidCourse_id(cid)?"000000":cid;
+        this.lecturer= !l.equalsIgnoreCase("")?l:"TBA";
+        max_students=30;
+        no_students=0;
     }
 
     public Course(String n, String cid, String l, int max) {
         this.course_name = !n.equalsIgnoreCase("")?n:"TBA";
         // implement the rest here
+    //    this.course_id=cid.equals("")?"000000":cid;
+        this.course_id=!isValidCourse_id(cid)?"000000":cid;
+        this.lecturer= !l.equalsIgnoreCase("")?l:"TBA";
+        this.max_students=(max>=10)?max:30;
+        this.no_students=0;
+     /*   course_name=n;
+        course_id=cid;
+        lecturer=l;
+        max_students=max;
+        no_students=0;*/
     }
-
     public String getCourse_name() {
         return course_name;
     }
@@ -35,46 +60,68 @@ public class Course {
     }
 
     public void setCourse_id(String course_id) {
+
         this.course_id = isValidCourse_id(course_id)?course_id:this.course_id;
+
     }
 
     // implement the other get and set methods here
     public String getLecturer() {
         // implement here
-        return "lecturer";
+        return lecturer;
     }
 
-    public void setLecturer(String lecturer) {
+    public void setLecturer(String l) {
         // implement here
+        this.lecturer = !l.equalsIgnoreCase("")?l:this.lecturer;
     }
 
     public int getMax_students() {
         // implement here
-        return 0;
+        return max_students;
     }
 
     public void setMax_students(int max_students) {
         // implement here
+        this.max_students= max_students>=30?max_students:this.max_students;
     }
 
     public int getNo_students() {
         // implement here
-        return 0;
+        return no_students;
     }
 
     public void setNo_students(int no_students) {
         // implement here
+        this.no_students= no_students<=max_students && no_students>=0?no_students:this.no_students;
     }
 
     @Override
     public String toString() {
-        String o = this.course_name + " ("
-                + this.course_id + "), Teacher: "
-                + this.lecturer + ", has ";
+        String o;
+        if(no_students==0) {
 
-        // implement the rest here
+             o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has NO" +
+                    " student, [maximum: 60]";
+        }
+        else if(no_students==1) {
 
-        return o;
+             o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has ONE" +
+                    " student, [maximum: 60]";
+        }
+        else {
+
+            o = this.course_name + " ("
+                    + this.course_id + "), Teacher: "
+                    + this.lecturer + ", has " +this.no_students+
+                    " students, [maximum: 60]";
+        }
+
+     return o;
     }
 
     private boolean isValidCourse_id(String id) {
@@ -83,10 +130,10 @@ public class Course {
 
         // implement the rest here
 
-        return true;
+        return m.matches();
     }
 
-    // Regular expression for the Student ID pattern
+    // Regular expression for the Student ID pattern \d= 0-9
     private static final String idREGEX = "\\d{6}";
 
     // all private attributes
--- src/cpe200/Student.java
+++ src/cpe200/Student.java
@@ -1,18 +1,38 @@
 package cpe200;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Student {
+    private  String std_name;
+    private  String student_id;
+    private  int yob;
+    private  boolean isActive;
+    private static final String idREGEX = "5+[6-9]+0+6+1+[0-2]+[0-9]+[0-9]+[0-9]";
 
     public Student() {
         // implement here
+        this("","",1990,false);
     }
-
+    public Student(String n,String id,int y,boolean s){
+        this.std_name = !n.equalsIgnoreCase("")?n:"John Doe";
+        this.student_id=!isValidCourse_id(id)?"560610000":id;
+        this.yob=(y>1990)?y:1990;
+        this.isActive=s;
+    }
     // implement all missing constructors here
 
     // implement all get and set methods here
 
     @Override
     public String toString() {
-        String o = "Something";
+        String o;
+        if(!isActive) {
+             o = std_name + " (" + student_id + ") was born in " + yob + " is an INACTIVE student.";
+        }
+        else{
+             o = std_name + " (" + student_id + ") was born in " + yob + " is an ACTIVE student.";
+        }
 
         return o;
     }
@@ -26,5 +46,44 @@ public class Student {
     }
 
     // declare your attributes here
+    public  String getName(){
+        return std_name;
+    }
+    public  void  setName(String std_name){
+        this.std_name = !std_name.equalsIgnoreCase("")?std_name:this.std_name;
+    }
+
+    public String getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(String student_id) {
+
+        this.student_id = isValidCourse_id(student_id)?student_id:this.student_id;
+    }
+
+    public int getYearOfBirth() {
+        return yob;
+    }
+
+    public void setYearOfBirth(int yob) {
+        this.yob= yob>1989?yob:this.yob;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+  //  "\\d{9}";
+    private boolean isValidCourse_id(String id) {
+        Pattern p = Pattern.compile(idREGEX);
+        Matcher m = p.matcher(id);
+
+             return m.matches();
+    }
 
 }
